1e8a3fcf404feeddb66c62add9b3f50b433da221
Oscar Berrocal <nicolasberrocal95@gmail.com>
2023-09-04 14:40:41 -0500

    un poco de animacion y rellenando las piezas

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,6 +8,7 @@ using UnityEngine.UIElements;
 
 public class Board : MonoBehaviour
 {
+    public float timeBetweenPieces = 0.05f;
 
     public int width;
     public int height;
@@ -35,10 +36,10 @@ public class Board : MonoBehaviour
 
         SetupBoard();
         PositionCamera();
-        SetupPieces();
+        StartCoroutine(SetupPieces());
     }
 
-    private void SetupPieces()
+    private IEnumerator SetupPieces()
     {
         int maxIterations = 50;
         int currentIteration = 0;
@@ -46,20 +47,25 @@ public class Board : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                currentIteration = 0;
-                var newPiece = CreatePieceAt(x, y);
-                while (HasPreviousMatches(x, y))
+                yield return new WaitForSeconds(timeBetweenPieces);
+                if (Pieces[x, y] == null)
                 {
-                    ClearPieceAt(x, y);
-                    newPiece = CreatePieceAt(x, y);
-                    currentIteration++;
-                    if (currentIteration > maxIterations)
+                    currentIteration = 0;
+                    var newPiece = CreatePieceAt(x, y);
+                    while (HasPreviousMatches(x, y))
                     {
-                        break;
+                        ClearPieceAt(x, y);
+                        newPiece = CreatePieceAt(x, y);
+                        currentIteration++;
+                        if (currentIteration > maxIterations)
+                        {
+                            break;
+                        }
                     }
                 }
             }
         }
+        yield return null;
     }
 
     private void ClearPieceAt(int x, int y)
@@ -109,25 +115,34 @@ public class Board : MonoBehaviour
 
     public void TileDown(Tile tile_)
     {
-        startTile = tile_;
+        if (!swappingPieces)
+        {
+            startTile = tile_;
+        }
     }
 
     public void TileOver(Tile tile_)
     {
-        endTile = tile_;
+        
+        {
+            endTile = tile_;
+        } 
     }
 
     public void TileUp(Tile tile_)
     {
-        if (startTile != null && endTile != null && IsCloseTo(startTile, endTile))
+        if (!swappingPieces)
         {
-            StartCoroutine(SwapTiles());
+            if (startTile != null && endTile != null && IsCloseTo(startTile, endTile))
+            {
+                StartCoroutine(SwapTiles());
+            }
         }
-        
     }
 
     IEnumerator SwapTiles()
     {
+        swappingPieces = true;
         var StarPiece = Pieces[startTile.x, startTile.y];
         var EndPiece = Pieces[endTile.x, endTile.y];
 
@@ -230,6 +245,11 @@ public class Board : MonoBehaviour
         {
             var newCollapsedPieces = collapseColumns(GetColumns(newMatches), 0.3f);
             FindMatchsRecursively(newCollapsedPieces);
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.1f);
+            StartCoroutine(SetupPieces());
         }
         yield return null;
     }
